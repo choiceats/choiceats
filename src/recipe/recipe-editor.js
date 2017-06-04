6ed679b1c931b54d9280341e00f5b6e29faf842c
Nathan Norton <nathan.norton@ave81.com>
2017-06-04 13:30:45 -0500

    Got draft.js going.

--- src/recipe/recipe-editor.js
+++ src/recipe/recipe-editor.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import {Editor, EditorState, RichUtils} from 'draft-js'
 import styled from 'styled-components'
+
 import './recipe-editor.css'
+import 'draft-js/dist/Draft.css'
 
 export class RecipeEditor extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      recipeName: EditorState.createEmpty(),
-      recipeIngredients: EditorState.createEmpty(),
-      recipeInstructions: EditorState.createEmpty()
+      editorState: EditorState.createEmpty()
     }
-    this.onChange = (key, value) => this.setState(() => ({[key]: value}))
+
+    this.focus = () => this.refs.editor.focus()
+    this.onChange = (editorState) => this.setState(() => ({editorState}))
+
+    this.handleKeyCommand = (command) => this._handleKeyCommand(command)
+    this.onTab = (e) => this._onTab(e)
+    this.toggleBlockType = (type) => this._toggleBlockType(type)
+    this.toggleInlineStyle = (style) => this._toggleInlineStyle(style)
   }
 
-  handleKeyCommand (command) {
-    const newState = RichUtils.handleKeyCommand(this.state.recipeName, command)
+  _handleKeyCommand (command) {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
     if (newState) {
       this.onChange(newState)
       return 'handled'
@@ -23,36 +30,134 @@ export class RecipeEditor extends React.Component {
     return 'not-handled'
   }
 
-  _onBoldClick () {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.recipeName, 'BOLD'))
+  _onTab (e) {
+    const maxDepth = 4
+    this.onChange(RichUtils.onTab(e, this.state.editorState, maxDepth))
+  }
+
+  _toggleBlockType (blockType) {
+    this.onChange(
+      RichUtils.toggleBlockType(
+        this.state.editorState,
+        blockType
+      )
+    )
+  }
+
+  _toggleInlineStyle (inlineStyle) {
+    this.onChange(
+      RichUtils.toggleInlineStyle(
+        this.state.editorState,
+        inlineStyle
+      )
+    )
   }
 
   render () {
+    const { editorState } = this.state
     return (
-      <EditorContainer>
+      <EditorContainer className='RichEditor-root'>
         <EditorTitle>Recipe Editor</EditorTitle>
-        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
-        <EditorLabel>Recipe Name</EditorLabel>
-        <Editor
-          editorState={this.state.recipeName}
-          onChange={(val) => this.onChange('recipeName', val)}
-          handleKeyCommand={this.handleKeyCommand}
-        />
-        <EditorLabel>Ingredients</EditorLabel>
-        <Editor
-          editorState={this.state.recipeIngredients}
-          onChange={(val) => this.onChange('recipeIngredients', val)}
-          handleKeyCommand={this.handleKeyCommand}
-        />
-        <EditorLabel>Instructions</EditorLabel>
-        <Editor
-          editorState={this.state.recipeInstructions}
-          onChange={(val) => this.onChange('recipeInstructions', val)}
-          handleKeyCommand={this.handleKeyCommand}
-        />
+
+        <BlockStyleControls
+          editorState={editorState}
+          onToggle={this.toggleBlockType}
+              />
+        <InlineStyleControls
+          editorState={editorState}
+          onToggle={this.toggleInlineStyle}
+              />
+        <div className='RichEditor-editor'>
+          <Editor
+            editorState={editorState}
+            onChange={this.onChange}
+            handleKeyCommand={this.handleKeyCommand}
+          />
+        </div>
       </EditorContainer>
     )
   }
+}
+
+class StyleButton extends React.Component {
+  constructor () {
+    super()
+    this.onToggle = (e) => {
+      e.preventDefault()
+      this.props.onToggle(this.props.style)
+    }
+  }
+
+  render () {
+    let className = 'RichEditor-styleButton'
+    if (this.props.active) {
+      className += ' RichEditor-activeButton'
+    }
+    return (
+      <span className={className} onMouseDown={this.onToggle}>
+        {this.props.label}
+      </span>
+    )
+  }
+}
+
+const BLOCK_TYPES = [
+  {label: 'H1', style: 'header-one'},
+  {label: 'H2', style: 'header-two'},
+  {label: 'H3', style: 'header-three'},
+  {label: 'H4', style: 'header-four'},
+  {label: 'H5', style: 'header-five'},
+  {label: 'H6', style: 'header-six'},
+  {label: 'Blockquote', style: 'blockquote'},
+  {label: 'UL', style: 'unordered-list-item'},
+  {label: 'OL', style: 'ordered-list-item'},
+  {label: 'Code Block', style: 'code-block'}
+]
+
+const BlockStyleControls = (props) => {
+  const {editorState} = props
+  const selection = editorState.getSelection()
+  const blockType = editorState
+          .getCurrentContent()
+          .getBlockForKey(selection.getStartKey())
+          .getType()
+  return (
+    <div className='RichEditor-controls'>
+      {BLOCK_TYPES.map((type) =>
+        <StyleButton
+          key={type.label}
+          active={type.style === blockType}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+              />
+            )}
+    </div>
+  )
+}
+
+var INLINE_STYLES = [
+  {label: 'Bold', style: 'BOLD'},
+  {label: 'Italic', style: 'ITALIC'},
+  {label: 'Underline', style: 'UNDERLINE'},
+  {label: 'Monospace', style: 'CODE'}
+]
+
+const InlineStyleControls = (props) => {
+  var currentStyle = props.editorState.getCurrentInlineStyle()
+  return (
+    <div className='RichEditor-controls'>
+      {INLINE_STYLES.map(type =>
+        <StyleButton
+          key={type.label}
+          active={currentStyle.has(type.style)}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+              />
+            )}
+    </div>
+  )
 }
 
 const EditorContainer = styled.div`
@@ -60,10 +165,6 @@ const EditorContainer = styled.div`
   width: 100%;
   max-width: 550px;
   border: 1px solid gray;
-`
-
-const EditorLabel = styled.label`
-  font-weight: bold;
 `
 
 const EditorTitle = styled.div`
