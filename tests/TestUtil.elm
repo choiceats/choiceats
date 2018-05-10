b0b3034e7e6bc411887c056674f92430691acef8
Nathaniel Allred <neallred@gmail.com>
2018-05-09 22:25:17 -0500

    inital tests for project

---
+++ tests/TestUtil.elm
@@ -0,0 +1,20 @@
+module TestUtil exposing (..)
+
+-- THIRD PARTY --
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+import Util exposing (getImageUrl)
+
+
+suite : Test
+suite =
+    describe "Utils"
+        [ describe "getImageUrl"
+            [ test "defaults to / for missing images" <|
+                \_ -> getImageUrl "" |> Expect.equal "/"
+            , test "returns string otherwise" <|
+                \_ -> getImageUrl "goats" |> Expect.equal "goats"
+            ]
+        ]
