879f1d8845b2b46f7222f171e470003c9a096290
neallred <neallred@gmail.com>
2018-07-16 18:13:09 -0500

    revert failed test

--- tests/TestUtil.elm
+++ tests/TestUtil.elm
@@ -15,6 +15,6 @@ suite =
             [ test "defaults to / for missing images" <|
                 \_ -> getImageUrl "" |> Expect.equal "/"
             , test "returns string otherwise" <|
-                \_ -> getImageUrl "goats" |> Expect.equal "goats!"
+                \_ -> getImageUrl "goats" |> Expect.equal "goats"
             ]
         ]
